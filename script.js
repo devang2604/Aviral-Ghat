5d3778c7cfb90fb360377d59b5dc153d12de4d88
devang2604 <imdevang04@gmail.com>
2024-01-21 00:48:59 +0530

    Ghats section

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ hamburger.onClick = function() {
     navBar = document.querySelector('.nav-bar');
     navBar.classList.toggle('active');
 }
+
+let next = document.querySelector('.next');
+let prev = document.querySelector('.prev');
+
+next.addEventListener('click', function() {
+    let ghats = document.querySelectorAll('.ghat');
+    document.querySelector('.slide').appendChild(ghats[0]);
+});
+
+prev.addEventListener('click', function() {
+    let ghats = document.querySelectorAll('.ghat');
+    document.querySelector('.slide').prepend(ghats[ghats.length - 1]);
+});
