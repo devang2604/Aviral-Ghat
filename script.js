9b3e5312e9754f85feec22a76d883277415908e9
devang2604 <imdevang04@gmail.com>
2024-01-22 02:36:45 +0530

    Contact Us and footer section added

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ hamburger.addEventListener('click', function() {
     let navBar = document.querySelector('.nav-bar');
     navBar.classList.toggle('active');
 });
+
+document.querySelector('#contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.target.elements.name.value = '';
+    e.target.elements.email.value = '';
+    e.target.elements.message.value = '';
+  });
