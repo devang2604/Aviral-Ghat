3c7ac456f3b492aa1323a5c60680e06aeca7d56c
devang2604 <imdevang04@gmail.com>
2024-02-03 01:44:10 +0530

    Changing Contact us

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let hamburger = document.querySelector('.hamburger');
-hamburger.addEventListener('click', function() {
-    let navBar = document.querySelector('.nav-bar');
-    navBar.classList.toggle('active');
+let hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", function () {
+  let navBar = document.querySelector(".nav-bar");
+  navBar.classList.toggle("active");
 });
 
-document.querySelector('#contact-form').addEventListener('submit', (e) => {
-    e.preventDefault();
-    e.target.elements.name.value = '';
-    e.target.elements.email.value = '';
-    e.target.elements.message.value = '';
-  });
+document.querySelector("#contact-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  e.target.elements.name.value = "";
+  e.target.elements.email.value = "";
+  e.target.elements.message.value = "";
+});
